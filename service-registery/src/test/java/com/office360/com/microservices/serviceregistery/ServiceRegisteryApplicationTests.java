f95e96420d97d55d50a8e975787e2f01d82de4d3
Karthick2412 <karthikgm2412@gmail.com>
2023-10-15 19:17:52 +0530

    Eureka Service Registery For office360 microservices

---
+++ service-registery/src/test/java/com/office360/com/microservices/serviceregistery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.office360.com.microservices.serviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
